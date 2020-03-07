78390c0f138715947b88e62b677d31accc9e5599
giselle <glj@holacons.com>
2020-03-07 01:36:41 +0100

    Create  connection with SQL server.

--- StudentManager.DataAccess.SQL/Repositories/StudentRepository.cs
+++ StudentManager.DataAccess.SQL/Repositories/StudentRepository.cs
@@ -1,14 +1,24 @@
 ﻿using StudentManager.Common.Aggregates.StudentAggregate;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 
 namespace StudentManager.DataAccess.SQL.Repositories
 {
     public class StudentRepository : IStudentRepository
     {
+        private readonly string connectionString = "Server=.;Database=students;User Id=sa;Password=yourStrong(!)Password;";
         public IEnumerable<Student> GetAll()
         {
-            throw new NotImplementedException();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (var command = new SqlCommand("select * from student", connection))
+                {
+                    var reader = command.ExecuteReader();
+                }
+            }
+            return null;
         }
 
         public Student Create(Student entity)
