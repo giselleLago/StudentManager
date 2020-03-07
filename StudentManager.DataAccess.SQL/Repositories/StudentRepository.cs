174383dc095667bc4701dbb2e86bb43044fdc905
giselle <glj@holacons.com>
2020-03-07 16:43:48 +0100

    GetAll() method finished.

--- StudentManager.DataAccess.SQL/Repositories/StudentRepository.cs
+++ StudentManager.DataAccess.SQL/Repositories/StudentRepository.cs
@@ -7,18 +7,36 @@ namespace StudentManager.DataAccess.SQL.Repositories
 {
     public class StudentRepository : IStudentRepository
     {
-        private readonly string connectionString = "Server=.;Database=students;User Id=sa;Password=yourStrong(!)Password;";
+        private readonly string connectionString = "Server=.;Database=StudentManager;User Id=sa;Password=yourStrong(!)Password;";
         public IEnumerable<Student> GetAll()
         {
+            var studentList = new List<Student>();
             using (var connection = new SqlConnection(connectionString))
             {
                 connection.Open();
-                using (var command = new SqlCommand("select * from student", connection))
+                using (var command = new SqlCommand("select * from Student", connection))
                 {
-                    var reader = command.ExecuteReader();
+                    using (var reader = command.ExecuteReader())
+                    {
+                        
+                        while (reader.Read())
+                        {
+                            var student = new Student
+                            {
+                                Id = int.Parse(reader["Id"].ToString()),
+                                Guid = Guid.Parse(reader["Guid"].ToString()),
+                                Name = reader["Name"].ToString(),
+                                LastName = reader["LastName"].ToString(),
+                                BirthDate = DateTime.Parse(reader["Birthday"].ToString())
+                            };
+                            studentList.Add(student);
+                        }
+
+                        connection.Close();
+                    }
                 }
             }
-            return null;
+            return studentList;
         }
 
         public Student Create(Student entity)
