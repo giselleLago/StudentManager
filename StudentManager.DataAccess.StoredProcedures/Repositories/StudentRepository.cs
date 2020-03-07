da776188493f4b8f6184fecec289f380e73a8f7b
Raúl Seguí <r.segui.t@gmail.com>
2020-03-07 22:00:37 +0100

    Created all methods needed for Stored Procedural. close #5 #8 #9 #10 #11 #13

--- StudentManager.DataAccess.StoredProcedures/Repositories/StudentRepository.cs
+++ StudentManager.DataAccess.StoredProcedures/Repositories/StudentRepository.cs
@@ -1,29 +1,120 @@
 ﻿using StudentManager.Common.Aggregates.StudentAggregate;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace StudentManager.DataAccess.StoredProcedures.Repositories
 {
     public class StudentRepository : IStudentRepository
     {
+        private readonly string connectionString = "Server=.;Database=StudentManager;User Id=sa;Password=yourStrong(!)Password;";
         public IEnumerable<Student> GetAll()
         {
-            throw new NotImplementedException();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                string procedure = @"dbo.[GetAllStudents]";
+                SqlCommand command = new SqlCommand(procedure, connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+                connection.Open();
+                var studentsList = ReadDatabase(command);
+
+                connection.Close();
+
+                return studentsList;
+
+            }
         }
 
         public Student Create(Student entity)
         {
-            throw new NotImplementedException();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                string procedure = @"dbo.[AddStudent]";
+                SqlCommand command = new SqlCommand(procedure, connection);
+                command.Parameters.AddWithValue("@student_name", entity.Name);
+                command.Parameters.AddWithValue("@student_surname", entity.LastName);
+                command.Parameters.AddWithValue("@student_birth_date", entity.BirthDate);
+                command.CommandType = CommandType.StoredProcedure;
+
+                connection.Open();
+
+                command.ExecuteNonQuery();
+
+                connection.Close();
+            }
+            return entity;
         }
 
         public Student Update(Student entity)
         {
-            throw new NotImplementedException();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                string procedure = @"dbo.[UpdateStudent]";
+                SqlCommand command = new SqlCommand(procedure, connection);
+                command.Parameters.AddWithValue("@student_id", entity.StudentId);
+                command.Parameters.AddWithValue("@student_name", entity.Name);
+                command.Parameters.AddWithValue("@student_surname", entity.LastName);
+                command.Parameters.AddWithValue("@student_birth_date", entity.BirthDate);
+                command.CommandType = CommandType.StoredProcedure;
+
+                connection.Open();
+
+                command.ExecuteNonQuery();
+
+                connection.Close();
+            }
+            return entity;
         }
 
         public void DeleteById(int id)
         {
-            throw new NotImplementedException();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                string procedure = @"dbo.[DeleteStudent]";
+                SqlCommand command = new SqlCommand(procedure, connection);
+                command.Parameters.AddWithValue("@student_id", id);
+                command.CommandType = CommandType.StoredProcedure;
+
+                connection.Open();
+
+                command.ExecuteNonQuery();
+
+                connection.Close();
+            }
+        }
+
+        private List<Student> ReadDatabase(SqlCommand command)
+        {
+            var reader = command.ExecuteReader();
+            var studentsList = new List<Student>();
+            if (reader.HasRows)
+            {
+                studentsList = ReadStudents(reader);
+            }
+            else
+            {
+                Console.WriteLine("No data found.");
+            }
+            reader.Close();
+            return studentsList;
+        }
+
+        private List<Student> ReadStudents(SqlDataReader reader)
+        {
+            var studentsList = new List<Student>();
+            while (reader.Read())
+            {
+                var student = new Student(
+                reader.GetInt32(0),
+                reader.GetString(1),
+                reader.GetString(2),
+                reader.GetDateTime(3)
+                );
+                studentsList.Add(student);
+            }
+            return studentsList;
         }
     }
 }
